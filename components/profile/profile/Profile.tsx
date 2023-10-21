589dd2256060786a00a90320675f8e986391af20
Olamide <simonadepetoye@gmail.com>
2023-10-21 13:44:39 +0100

    --empty

--- components/profile/profile/Profile.tsx
+++ components/profile/profile/Profile.tsx
@@ -5,13 +5,11 @@ import { images } from '../../../constants'
 import { AntDesign } from '@expo/vector-icons'
 import { useRouter } from 'expo-router'
 import countries from '../../../data/countries.json'
-import SvgUri from 'react-native-svg'
 
 const ProfileHeader = () => {
   const router = useRouter()
   const fetchImage = () => {
     const country = countries.find(({ name }) => name === 'Nigeria')
-    // console.log(fetchImage)
     return country?.flag
   }
 
@@ -26,16 +24,6 @@ const ProfileHeader = () => {
           <View style={styles.rowContainer}>
             <AntDesign name="user" size={16} color="#292D32" />
             <Text style={styles.gender}>Female</Text>
-          </View>
-          <View style={styles.rowContainer}>
-            {/* Nigeria Icon */}
-            <SvgUri  />
-            {/* <Svg
-              source={{ uri: 'https://flagcdn.com/ng.svg' }}
-              style={{ width: 16, height: 16 }}
-            /> */}
-            {/* <AntDesign name="user" size={16} color="#292D32" /> */}
-            <Text style={styles.country}>Nigeria</Text>
           </View>
         </View>
       </View>
